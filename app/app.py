11ca9ca3f84e48c11aa4fd808753cfad703a8090
Prakruthisb <147960787+Prakruthisb@users.noreply.github.com>
2026-03-14 07:49:42 +0530

    fix database url encoding

--- app/app.py
+++ app/app.py
@@ -8,9 +8,17 @@ import os
 # -----------------------------
 # Database Connection
 # -----------------------------
+# engine = create_engine(
+#     f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+#     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+#     connect_args={"sslmode": "require"},
+#     pool_pre_ping=True
+# )
+
+DATABASE_URL = "postgresql+psycopg2://postgres.oatlsxbwiloerlfhdtby:psbcloud%402026@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
+
 engine = create_engine(
-    f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+    os.getenv("DATABASE_URL"),
     connect_args={"sslmode": "require"},
     pool_pre_ping=True
 )
