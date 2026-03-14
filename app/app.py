3787960c6c00515976a2be25c782a4b160abfd6a
Prakruthisb <147960787+Prakruthisb@users.noreply.github.com>
2026-03-14 07:32:39 +0530

    fix supabase ssl connection

--- app/app.py
+++ app/app.py
@@ -10,7 +10,9 @@ import os
 # -----------------------------
 engine = create_engine(
     f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
 )
 
 # -----------------------------
@@ -18,7 +20,7 @@ engine = create_engine(
 # -----------------------------
 @st.cache_data
 def load_movies():
-    query = "SELECT * FROM movies;"
+    query = "SELECT * FROM public.movies;"
     df = pd.read_sql(query, engine)
 
     # Convert embeddings to numpy arrays
